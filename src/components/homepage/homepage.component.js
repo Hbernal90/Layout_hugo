9655cccfffa59c5c1a0825b852e0092f92945fa2
Axel Malagon <jmalagon@intersysconsulting.com>
2020-01-20 16:49:10 -0600

    added db, and material-table library

--- src/components/homepage/homepage.component.js
+++ src/components/homepage/homepage.component.js
@@ -1,18 +1,56 @@
 import React, { Component } from 'react';
-import logo from '../../logo.svg';
+import MaterialTable from 'material-table';
+import { employees, floors } from '../../db/db';
+import layout from '../../assets/layout3.jpg';
 
-import  './homepage.styles.css';
+import SelectButton from '../select-button/select-button';
+
+
+import './homepage.styles.css';
 
 export class Homepage extends Component {
+    constructor() {
+        super();
+        this.state = {
+            user: "Alfredo Sanchez",
+            floor: floors,
+            floorSelected: 24
+        }
+    }
+
+    handleSelect = () => {
+        console.log("WORKS!!!!")
+    }
+
     render() {
         return (
-            <div>
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </header>
+            <div className="homepage-container">
+
+                <SelectButton floor={this.state.floor} handle={this.handleSelect} />
+                <div className="homepage-image">
+                    <img src={layout} alt="alt" />
+                </div>
+                <br />
+                <br /> <br /> <br />
+                <div className="">
+                    <MaterialTable
+                        options={{
+                            exportButton: true
+                        }}
+                        columns={[
+                            { title: 'Name', field: 'name' },
+                            { title: 'Building', field: 'building' },
+                            { title: 'Floor', field: 'floor', type: 'numeric' }
+                        ]}
+                        data={employees}
+                        title="LAYOUT SYSTEM"
+                    />
+
+                </div>
+
             </div>
         )
     }
 }
 
-export default Homepage
+export default Homepage;
---
+++ src/db/db.js
@@ -0,0 +1,6 @@
+export const employees = [
+    { name: 'Alfredo Sanchez', building: 'MDC', floor: 24, seat:'A-5' },
+    { name: 'Axel Malagon', building: 'MDC', floor: 18 },
+    { name: 'David Barajas', building: 'MDC', floor: 18 }
+]
+export const  floors = [0]
