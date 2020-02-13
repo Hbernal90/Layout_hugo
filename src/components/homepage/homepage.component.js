67a2a785a0ee4ee80a9569879c598aca6abe2a86
Alejandro Venegas <avenegas@intersysconsulting.com>
2020-02-13 12:33:08 -0600

    Bug fix

--- src/components/homepage/homepage.component.js
+++ src/components/homepage/homepage.component.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux'
 import MaterialTable from 'material-table';
 import { employees } from '../../db/db';
 
 import SelectButton from '../select-button/select-button';
+import { filterLocation } from '../../redux/home/home.action'
 
 
 import './homepage.styles.css';
 
-function Homepage() {
+function Homepage({filterLocation, filters}) {
     const [state, setState] = useState({
         user: "Alfredo Sanchez",
         filters: {
@@ -58,12 +60,17 @@ function Homepage() {
                 [evt.target.name]: value
             }
         })
+        console.log("handle", state.filters)
+        filterLocation({
+            ...state.filters,
+            [evt.target.name]: value
+        });
     }
 
 
     return (
         <div className="homepage-container">
-            <SelectButton name='location' inputLabel={"Locations"} items={Object.keys(state.comboBox.locations)} handleSelect={handleSelect} itemsSelected={state.filters.location} />
+            <SelectButton name='location' inputLabel={"Locations"} items={Object.keys(state.comboBox.locations)} handleSelect={handleSelect} itemsSelected={filters.location} />
             <SelectButton name='floor' inputLabel={"Floors"} items={Object.keys(state.comboBox.locations[state.filters.location].floors)} handleSelect={handleSelect} itemsSelected={state.filters.floor} />
             <SelectButton name='section' inputLabel={"Section"} items={state.comboBox.locations[state.filters.location].floors[state.filters.floor].sections} handleSelect={handleSelect} itemsSelected={state.filters.section} />
             <div className="homepage-image">
@@ -89,4 +96,15 @@ function Homepage() {
     )
 }
 
-export default Homepage;
+const mapDispatchToProps = dispatch => ({
+    filterLocation: (filters) => dispatch(filterLocation(filters))
+})
+
+const mapStateToProps = state => {
+    console.log(state)
+    return{
+        filters: state.filters
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Homepage);
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,14 +2,15 @@ import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
-import  homeReducer from './home/home.reducer';
+import homeReducer from './home/home.reducer';
 
-const persystConfig = {
-    key: 'root',
+const persistConfig = {
+    key: 'filters',
     storage,
-    whitelist: []
+    whitelist: ['home']
 }
 
-const rootReducer = combineReducers({home: homeReducer})
+// TODO
+//const rootReducer = combineReducers({home: homeReducer})
 
-export default persistReducer(persystConfig, rootReducer);
+export default persistReducer(persistConfig, homeReducer);
