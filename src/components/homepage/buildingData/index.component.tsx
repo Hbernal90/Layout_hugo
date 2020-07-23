ae7ac2eac7e99127db8429c132a474e9be75eff3
Jorge Garcia <jogadev@outlook.com>
2020-07-23 13:54:00 -0500

    Building info pane completed with sample data

--- src/components/homepage/buildingData/index.component.tsx
+++ src/components/homepage/buildingData/index.component.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import phoneIcon from "../../../assets/icons/phone.svg"
 import cursorIcon from "../../../assets/icons/cursor.svg"
+import layerIcon from "../../../assets/icons/layer.svg";
+import charIcon from "../../../assets/icons/chair.svg";
+import workIcon from "../../../assets/icons/work.svg";
+
+
 const data = {
     title: "Punto Sao Paulo",
     description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Necessitatibus earum aperiam aut cupiditate impedit, minima praesentium, architecto perspiciatis corrupti soluta tempora quasi possimus? Itaque, facere? Harum provident suscipit beatae quaerat.",
@@ -19,9 +24,11 @@ function BuildingData(props: any) {
             <div className="iconAndText"><img src={cursorIcon} alt="" /> <span>{data.address}</span></div>
             <div className="iconAndText"><img src={phoneIcon} alt="" /><span>{data.phone}</span></div>
             <hr />
-            <span className="seatsInfo">Total floors: {data.totalFloors}</span>
-            <span className="seatsInfo">Total seats: {data.totalSeats}</span>
-            <span className="seatsInfo">Available seats: {data.availableSeats}</span>
+            <div className="seatsWrapper">
+                <div className="iconAndText"><img src={layerIcon} alt="" /><span>Total floors: {data.totalFloors}</span></div>
+                <div className="iconAndText"><img src={workIcon} alt="" /><span>Total seats: {data.totalSeats}</span></div>
+                <div className="iconAndText"><img src={charIcon} alt="" /><span>Available seats: {data.availableSeats}</span></div>
+            </div>
         </React.Fragment>
     )
 }
